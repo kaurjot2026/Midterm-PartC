ab93fc5366d9338967666c18821a720a4ee9d94e
kaurjot2026 <kau15601@sheridancollege.ca>
2024-02-12 09:39:00 -0500

    version 1 student details

--- src/days/Days.java
+++ src/days/Days.java
@@ -22,13 +22,12 @@ public class Days {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         
-    Scanner in =new Scanner(System.in);
-    System.out.println("Enter the day number (one,two,three,four,five) in string");
-    String code = in.next();
-    Weekdays t= new Weekdays();
-    t.nameOfDay(code); 
-    }// TODO code application logic here
-    
-    
+        for (Day day : EnumSet.allOf(Day.class)) {
+            printDay(day);
+        }
+    }
+
+    private static void printDay(Day day) {
+        System.out.println(day);
+    }
 }
